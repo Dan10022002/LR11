68fb0c396ec72b099d949f957201c1e3e5456130
Dan10022002 <70635329+Dan10022002@users.noreply.github.com>
2020-12-23 14:03:18 +0300

    Update multistream.cpp

--- multistream.cpp
+++ multistream.cpp
@@ -4,14 +4,14 @@
 #include <chrono>
 #include <thread>
 
-void Find_max_element(std::vector <int> Vec, std::vector <int> Block)
+void Find_max_element(std::vector <int> Vec, int low_border, int high_border, std::vector <int> Block)
 {
-	int max_element = Vec[0];
-	for (int h = 1; h < 5000; h++)
+	int max_element = Vec[low_border];
+	while ((low_border + 1) != high_border)
 	{
-		if (Vec[h] > max_element)
+		if (Vec[low_border + 1] > max_element)
 		{
-			max_element = Vec[h];
+			max_element = Vec[low_border + 1];
 		}
 	}
 	Block.push_back(max_element);
@@ -20,7 +20,7 @@ void Find_max_element(std::vector <int> Vec, std::vector <int> Block)
 int Find_min_element(std::vector <int> Vec)
 {
 	int min_element = Vec[0];
-	for (int h = 1; h < 5000; h++)
+	for (int h = 1; h < 10; h++)
 	{
 		if (Vec[h] < min_element)
 		{
@@ -30,49 +30,34 @@ int Find_min_element(std::vector <int> Vec)
 	return min_element;
 }
 
-void Initialization(std::vector <int> Vec, std::vector <int> Block, int n)
-{
-	int low_border = 5000 * n;
-	int high_border = 4999 + 5000 * n;
-	int counter = 0;
-	do
-	{
-		Block[counter] = Vec[low_border];
-		low_border += 1;
-		counter += 1;
-	} while (low_border != high_border);
-}
-
 int main()
 {
 	std::srand(std::time(nullptr));
 	std::vector <int> Vec;
-	for (int h = 0; h < 100000; h++)
+	for (int h = 0; h < 1000000; h++)
 	{
 		int m = std::rand();
 		Vec.push_back(m);
 	}
-	std::vector<std::vector<int>> Block(20, std::vector<int>(5000)); //вектор 20 интовых блоков размером 5000
 	std::vector <int> Block_rez;
 	auto start = std::chrono::system_clock::now();
-	for (int h = 0; h < 20; h++)
+	std::vector <std::thread*> Thread(10); //вектор 10 потоков
+	for (int h = 0; h < 10; h++)
 	{
-		std::vector <int> Block_time;
-		Initialization(Vec, Block_time, h);
-		Block.push_back(Block_time);
+		Thread[h] = new std::thread (Find_max_element, 10000*h, 9999 + 10000 * h, Block_rez);
 	}
-	std::vector <std::thread> Thread(20); //вектор 20 потоков
-	for (int h = 0; h < 20; h++)
+	for (int h = 0; h < 10; h++)
 	{
-		std::thread Thread_time(Find_max_element, Block[h], Block_rez);
-	}
-	for (int h = 0; h < 20; h++)
-	{
-		Thread[h].join();
+		Thread[h] -> join();
 	}
 	int rezult = Find_min_element(Block_rez);
 	auto end = std::chrono::system_clock::now();
 	auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
 	std::cout << "The result is " << rezult << "\n";
 	std::cout << "The working time of the program is " << time.count() << " nanoseconds." << '\n';
+	for (int h = 0; h < 10; h++)
+	{
+		delete Thread[h];
+	}
+	return 0;
 }
